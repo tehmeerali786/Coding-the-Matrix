85629354a5b1fb451fe89fb30b800802fa7e5849
tehmeerali786 <freelanceali786@gmail.com>
2025-05-15 01:56:55 +0500

    Chapter 1 4

--- dictutil.py
+++
@@ -1,5 +0,0 @@
-def dict2list(dct, keylist): return [dct[i] for i in keylist]
-
-def list2dict(L, keylist): return  {j:i for i,j in zip(L, keylist)}
-
-def listrange2dict(L): return {i:L[i] for i in range(len(L))}
